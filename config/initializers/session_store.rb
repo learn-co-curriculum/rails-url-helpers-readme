9017a155d3f8290b7350cb8f52be4d08e9baf313
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 17:05:34 -0500

    rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_blog-flash_session'
+Rails.application.config.session_store :cookie_store, key: '_blog_flash_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
